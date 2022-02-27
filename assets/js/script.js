461e6e0bf70599af7cbe96086ac4666b8a8a3ee3
Paquito81 <hepalacios503@gmail.com>
2022-02-27 09:42:32 -0700

    create questionnaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+var questions = [
+    {
+        title:"What is the correct Javascript syntax to write 'Hello World'?",
+        choices: ["System.out.printin('Hello World')", "printing ('Hello World')","document.write('Hello World'), respond.write('Hello World')"],
+        answer: "document.write('Hello World')"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What are variables used for in Javascript Programs?",
+        choices: ["Storing numbers, dates ,or other values", "varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        answer: "Storing numbers,dates, or other values"
+    },
+    {
+        title: "Which of the following are capabilities of functions in JavaScript?",
+        choices: ["Return a value", "Accept parameters and Return value", "Accept parameters", "None of the above"],
+        answer: "Accept parameters"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
 
+];
+// Declared variables
